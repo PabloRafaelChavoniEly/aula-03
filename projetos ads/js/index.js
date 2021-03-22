8a70c0cc84549d2f42ca927b738014df1bbb610f
PABLO RAFAEL CHAVONI ELY <80414553+PabloRafaelChavoniEly@users.noreply.github.com>
2021-03-22 19:00:18 -0400

    Add files via upload

---
+++ projetos ads/js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(
+    function(){
+        alert("o documento carregou, fununcia!")
+    }
+)
+
+$("#bt_inicio").click(
+    function(){
+        alert('opa, você clicou no Botão Início');
+    }
+)
+
